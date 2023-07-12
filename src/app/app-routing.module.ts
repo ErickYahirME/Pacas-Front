b5c22fc779a0aa815b2e3d6f14ed0eda78ef4b99
Erick Yahir Meza Espina <espinayahir@gmail.com>
2023-07-12 08:59:34 -0600

    Agregando login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path: '', component: LoginComponent},
+    {path: 'registro', component: RegistroComponent}
+    
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
