f242c2ae493a75f7ad7e47f9af17d15192e13518
RodrigoU1 <lolrodrigouriel@gmail.com>
2023-07-14 07:14:08 -0600

    First commit Creación de "Carrito" y "Compras"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ComprasComponent } from './compras/compras.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'carrito',
+    component: CarritoComponent
+  },
+  {
+    path: 'compras',
+    component: ComprasComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ComprasComponent } from './compras/compras.component';
+import { RouterModule } from '@angular/router';
+import { CardCarritoComponent } from './card-carrito/card-carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarritoComponent,
+    ComprasComponent,
+    CardCarritoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
