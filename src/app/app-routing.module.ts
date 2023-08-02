d9683f07c469b2169ef6a93bd61469fe0cfa5eb3
JonixL <jesusjonatanhernandezgarcia@gmail.com>
2023-08-02 11:34:04 -0600

    Creacion de diseño CRUD de ropa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { RecuperarPassComponent } from './recuperar-pass/recuperar-pass.componen
 import { VentasPComponent } from './ventas-p/ventas-p.component';
 import { FooterComponent } from './footer/footer.component';
 import { ListaClotheComponent } from './Crud-Clothes/lista-clothe/lista-clothe.component';
+import { AddClotheComponent } from './Crud-Clothes/add-clothe/add-clothe.component';
+import { ModificClotheComponent } from './Crud-Clothes/modific-clothe/modific-clothe.component';
 
 const routes: Routes = [
   {path:'recuperarpass', component: RecuperarPassComponent},
   {path:'ventasp', component: VentasPComponent},
   {path:'footer', component:FooterComponent},
-  {path:'listasClothe', component:ListaClotheComponent}
+  {path:'listasClothe', component:ListaClotheComponent},
+  {path:'add-Clothe', component:AddClotheComponent},
+  {path:'modificropa', component:ModificClotheComponent},
+  {path:'listaderopa', component:ListaClotheComponent}
+
+
+
 
 ];
 
