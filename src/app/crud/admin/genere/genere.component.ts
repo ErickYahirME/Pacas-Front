d43a62021e661ea534ca7974a9a446224f767ea3
RodrigoU1 <lolrodrigouriel@gmail.com>
2023-08-01 20:14:00 -0600

    Creación de imagenes para carrusel

--- src/app/crud/admin/genere/genere.component.ts
+++ src/app/crud/admin/genere/genere.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GenereAdminService } from '../../../service/admin-Service/genere-admin.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-genere',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./genere.component.css']
 })
 export class GenereComponent {
+
+  constructor(
+    private fb: FormBuilder,
+    private GenereAdminService: GenereAdminService,
+    private Router: Router, 
+  ) { }
+
+  productoForm : FormGroup = this.fb.group({
+    sex: ['', [Validators.required, Validators.minLength(1)]],
+  });
+
+
+  save(){
+    console.log(this.productoForm.value);
+    console.log("Se guardó");
+    
+  }
 
 }
